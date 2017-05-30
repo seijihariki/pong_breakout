d6c6b64fc0cdd06867bb889218af472c37030e9f
Victor Seiji Hariki <victorseijih@gmail.com>
2017-05-30 17:11:05 -0300

    Bars are now physical

--- assets/scripts/ball.gd
+++ assets/scripts/ball.gd
@@ -1,29 +1,16 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 # class member variables go here, for example:
-var lvel = Vector2(300, 300)
-var acc = 5
-
-var avel = 5
+# var a = 2
+# var b = "textvar"
+var acc = 50
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	set_linear_velocity(Vector2(randi()%1000 - 500, randi()%1000 - 500))
 	set_fixed_process(true)
 	pass
 
 func _fixed_process(delta):
-	lvel += lvel.normalized()*acc*delta
-	
-	move(lvel*delta)
-	rotate(avel*delta)
-	
-	# Collision handling
-	if is_colliding():
-		var normal = get_collision_normal()
-		if normal != Vector2(0, 0):
-			lvel -= 2*normal*normal.dot(lvel)
-			pass
-		pass
-	
-	pass
+	set_applied_force(get_linear_velocity().normalized()*acc)
---
+++ assets/scripts/bar.gd
@@ -0,0 +1,46 @@
+extends RigidBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+const SPEED_FACTOR = 64
+
+export(int) var speed = 500000
+export(String) var up_button = "ui_up"
+export(String) var down_button = "ui_down"
+export(String) var activate_button = "ui_accept"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	set_process_input(true)
+	set_fixed_process(true)
+	pass
+
+var up = false
+var down = false
+var space = false
+
+func _input(event):	
+	if event.is_action_pressed(up_button):
+		apply_impulse(Vector2(0, 0), Vector2(0, -speed))
+		up = true
+	if event.is_action_released(up_button):
+		apply_impulse(Vector2(0, 0), Vector2(0, speed))
+		set_linear_velocity(Vector2(0, 0))
+		up = false
+	if event.is_action_pressed(down_button):
+		apply_impulse(Vector2(0, 0), Vector2(0, speed))
+		down = true
+	if event.is_action_released(down_button):
+		apply_impulse(Vector2(0, 0), Vector2(0, -speed))
+		set_linear_velocity(Vector2(0, 0))
+		down = false
+	if event.is_action_pressed(activate_button):
+		space = true
+	if event.is_action_released(activate_button):
+		space = false
+	pass
+
+func _fixed_process(delta):
+	pass
