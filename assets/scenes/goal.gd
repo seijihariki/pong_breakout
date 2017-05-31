f4ea517636049523651b540a34431855763aa391
Bruno Scholl <bruno.scholl@usp.br>
2017-05-30 22:27:32 -0300

    adds goal

---
+++ assets/scenes/goal.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+export(int) var p1_score 
+export(int) var p2_score
+export(int) var player_side
+
+func _on_Area2D_body_enter( body ):
+	if (player_side == 1):
+		p2_score = p2_score + 1
+	else:
+		p1_score = p1_score + 1
+	pass # replace with function body
