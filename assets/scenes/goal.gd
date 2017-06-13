ddb9e71e85386271002fbbed85a90235c6c17dab
Bruno Scholl <bruno.scholl@usp.br>
2017-06-13 17:06:32 -0300

    change text size and position and now only balls score goals

--- assets/scenes/goal.gd
+++ assets/scenes/goal.gd
@@ -3,7 +3,7 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
+var ball = preload('res://assets/scripts/ball.gd')
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -14,11 +14,12 @@ export(int) var p2_score = 0
 export(int) var player_side
 
 func _on_Area2D_body_enter( body ):
-	if (player_side == 1):
-		p2_score = p2_score + 1
-	else:
-		p1_score = p1_score + 1
-	pass # replace with function body
+	if (body extends ball):
+		if (player_side == 1):
+			p2_score = p2_score + 1
+		else:
+			p1_score = p1_score + 1
+			 # replace with function body
 
 func get_score():
 	return Vector2(p1_score, p2_score)
